09c684b93465a6947b98b3e9bd3f2e9dec3e4f6f
Collin McIntyre <collin.mcintyre@uwaterloo.ca>
2013-07-04 15:35:55 -0400

    fixed herbs/new, added recent herbs

--- app/helpers/herbs_helper.rb
+++ app/helpers/herbs_helper.rb
@@ -1,2 +1,11 @@
 module HerbsHelper
+  
+  def herb_image_tag(herb)
+    if !herb.photo_file_name.nil?
+		  image_tag herb.photo.url, :width => "200"
+		else
+      content_tag :small, "no image"
+		end
+  end
+  
 end
--- app/models/herb.rb
+++ app/models/herb.rb
@@ -1,6 +1,6 @@
 class Herb < ActiveRecord::Base
   
-  attr_accessible :name
+  attr_accessible :name, :family, :genus, :species, :author, :comments, :photo
   has_attached_file :photo
 
   def self.search(search)  
