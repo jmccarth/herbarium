9d84176fa6c44a0969ec0a0cbfffc7d29a523948
Collin McIntyre <collin.mcintyre@uwaterloo.ca>
2013-09-27 13:11:39 -0400

    config.relative_url_root = /herbarium

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,9 @@ module Herbarium
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    # Subdirectory relative URLs
+    config.relative_url_root = "/herbarium"
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
